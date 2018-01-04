9b79425df659749b46745025d00295810a032e99
Pratik Parmar <prateek18597@gmail.com>
2018-01-04 18:23:52 +0530

    First Commit

---
+++ src/iitj/IITJ.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iitj;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author SCEA
+ */
+public class IITJ {
+
+    public static String url = "jdbc:mysql://localhost:3306/lib";
+    public static String pswd = "1111";
+    public static String username = "root";
+    public static int day;
+  
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+}
